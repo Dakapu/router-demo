7365f1c14995b34030819f0208c3788b514f6b3c
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-01 22:12:21 +0200

    implement query strings for search parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import queryString from "query-string";
 
 export default function Posts() {
+  const locationSearch = queryString.parse(location.search);
+
   let params = useParams();
+  //?sortBy=newest&approved=true
+
   return (
     <div>
       <h1>Posts</h1>
