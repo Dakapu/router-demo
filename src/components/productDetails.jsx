416793456b992b4bbfdfd94998c48180b76ce794
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-01 22:29:33 +0200

    use useNavigate to redirect and replace paths

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Navigate } from "react-router-dom";
 
 export default function ProductDetails() {
   let params = useParams();
+  let history = useNavigate();
+
+  async function handleSave() {
+    //useful for logins, so users cannot go back to the login-page after they've already logged in
+    history("/products", { replace: true });
+  }
+
   return (
     <div>
       <h1>Product Details - {params.id}</h1>
-      <button onClick={handleSave()}>Save</button>
+      <button onClick={handleSave}>Save</button>
     </div>
   );
-}
-
-function handleSave() {
-  return null;
 }
